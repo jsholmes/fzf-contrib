6a3c5347d7ac333a5059b5f4106979bd72f9abbc
jsholmes <jsholmes@alum.mit.edu>
2015-07-17 14:41:25 -0400

    Update common.sh
    Change the default command to use git, then ag, then find

--- shell/common.sh
+++ shell/common.sh
@@ -1,3 +1,3 @@
 # Setting ag as the default source for fzf
-export FZF_DEFAULT_COMMAND='ag -l -g ""'
+export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD ||  ag -l -g ""' ||  find * -name ".*" -prune -o -type f -print -o -type l -print) 2> /dev/null
 export FZF_DEFAULT_OPTS='-x -m --sort 10000'
